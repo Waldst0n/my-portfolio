134da46e47b98bbb38f5c292ecb70ee24f5783bf
Waldst0n <waldston.ws@gmail.com>
2024-09-09 06:44:38 -0300

    Created Curriculum Link add images from project and Componernts adjusts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,33 @@
 import { NavLink } from 'react-router-dom'
 import image from '../img/1694174514925.jpeg'
 
+import { HiOutlineDownload } from "react-icons/hi";
+
+
 const Navbar = () => {
   return (
     <nav className='bg-tertiaryColor w-screen h-16 flex items-center fixed justify-between  mb-3'>
-        <div className='flex items-center gap-4 px-5 py-3'>
+        <div className='flex items-center justify-between w-full  gap-2 px-5 py-3'>
             <NavLink className='flex items-center gap-4' to='/'>
                 <img className='size-12 rounded-full' src={image} alt="" />
                 <h1 className='font-bold'>Waldston Santana</h1>
             
             </NavLink>
-        </div>
-        <div >
-            <ul className='flex gap-12'>
-                <li>
-                    <NavLink to='about'><p className='rounded-none py-2 px-4 hover:transition-all hover:delay-75 hover:rounded-2xl hover:bg-pink hover:text-tertiaryColor '>About</p></NavLink>
-                </li>
-                <li>
+        
+        
+            
+            <ul className='flex gap-10 items-center  '>
+            <li>
                     <NavLink to='projects'><p className='rounded-none py-2 px-4 hover:transition-all hover:delay-75 hover:rounded-2xl hover:bg-pink hover:text-tertiaryColor '>Projetos</p></NavLink>
                 </li>
+                <li>
+                    <a className=' flex gap-2 items-center rounded-none py-2 px-4 hover:transition-all hover:delay-75 hover:rounded-2xl hover:bg-pink hover:text-tertiaryColor ' href="https://drive.google.com/file/d/1Wp87K9Npz4H2YCxwVtY5e0FAhtgTOr-z/view?usp=drive_link" target='_blank' >Currículo<HiOutlineDownload /></a>
+                </li>
+                
             </ul>
+        
         </div>
-        <div>
-
-        </div>
+       
     </nav>
   )
 }
--- src/routes/Projects/Projects.jsx
+++ src/routes/Projects/Projects.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import image from '../../img/images.jpg'
+import geedu from '../../img/Captura de tela 2024-09-08 181012.png'
 import Card from '../../components/Card/Card'
+
+import { FaHtml5 } from "react-icons/fa";
+
 
 const Projects = () => {
   return (
@@ -11,8 +15,9 @@ const Projects = () => {
       <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-5 '>
         <Card 
         image={image}
-        title="Meu projeto 1"
-        description="Site institucional criado com React" />
+        title="Geedu"
+        description="Página de Login Simples com HTML e CSS"
+         />
          <Card 
         image={image}
         title="Meu projeto 1"
@@ -35,7 +40,7 @@ const Projects = () => {
         description="Site institucional criado com React" />
       </div>
 
-      <a className='text-tertiaryColor p-4 h-2 bg-pink  font-bold mt-4 flex items-center justify-center transition-all hover:delay-75 hover:rounded-xl' href="https://github.com/Waldst0n?tab=repositories" target='_blank'>Ver Todos</a>
+      <a className='text-tertiaryColor p-4 mb-4 h-2 bg-pink  font-bold mt-4 flex items-center justify-center transition-all hover:delay-75 hover:rounded-xl' href="https://github.com/Waldst0n?tab=repositories" target='_blank'>Ver Todos</a>
       
 
       

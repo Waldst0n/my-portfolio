5c5ae9a9a45228feef542c9f492070a724fa6e79
Waldst0n <waldston.ws@gmail.com>
2024-09-30 11:24:30 -0300

    fix: projects format correction

--- src/routes/Projects/Projects.jsx
+++ src/routes/Projects/Projects.jsx
@@ -14,19 +14,6 @@ const Projects = () => {
                 </h1>
             </div>
 
-            <h1 className="text-tertiaryColor mb-4 text-2xl">
-                Estou trabalhando nisso! Clique no botão ver todos para seguir
-                até meus repositórios.
-            </h1>
-
-            <a
-                className="text-tertiaryColor p-4 mb-14 h-2 bg-pink  font-bold mt-4 flex items-center justify-center transition-all hover:delay-75 hover:rounded-xl"
-                href="https://github.com/Waldst0n?tab=repositories"
-                target="_blank"
-            >
-                Ver Todos
-            </a>
-
             <h1 className="text-tertiaryColor text-2xl mb-14 ">
                 Segue abaixo o design pronto para visualização
             </h1>
@@ -63,6 +50,13 @@ const Projects = () => {
                     description="Site institucional criado com React"
                 />
             </div>
+            <a
+                className="text-tertiaryColor p-4 mb-14 h-2 bg-pink  font-bold mt-4 flex items-center justify-center transition-all hover:delay-75 hover:rounded-xl"
+                href="https://github.com/Waldst0n?tab=repositories"
+                target="_blank"
+            >
+                Ver Todos
+            </a>
         </div>
     )
 }
